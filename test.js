b97536165b7dc7f0f8c19f9eaf7048fe08d70c4c
a-ogilvie <alexander.ogilvie@icloud.com>
2019-05-24 15:29:14 +0900

    fix: correct failing test

--- test.js
+++ test.js
@@ -1,69 +1,80 @@
-var assert = require('assert');
-var batchPromises = require('./index.js');
+'use strict';
 
-describe('batchPromises', function() {
-  it('should return an array of results', function() {
-    return batchPromises(2, [1,2,3,4,5], function(i) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          resolve(i)
-        }, 100);
-      });
-    })
-    .then(function(res) {
-      assert.deepEqual(res, [1,2,3,4,5]);
+const assert = require('assert');
+const batchPromises = require('./index.js');
+
+describe('batchPromises', () => {
+  it('should return an array of results', () => {
+    return batchPromises(
+      2,
+      [1, 2, 3, 4, 5],
+      (i) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(i);
+          }, 10);
+        })
+    ).then((res) => {
+      assert.deepEqual(res, [1, 2, 3, 4, 5]);
     });
   });
 
-  it('should work with a promise returning an array', function() {
-    return batchPromises(2, Promise.resolve([1,2,3,4,5]), function(i) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          resolve(i)
-        }, 100);
-      });
-    })
-    .then(function(res) {
-      assert.deepEqual(res, [1,2,3,4,5]);
+  it('should work with a promise returning an array', () => {
+    return batchPromises(
+      2,
+      Promise.resolve([1, 2, 3, 4, 5]),
+      (i) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(i);
+          }, 10);
+        })
+    ).then((res) => {
+      assert.deepEqual(res, [1, 2, 3, 4, 5]);
     });
   });
 
-  it('should batch promises', function() {
-    var mostConcurrent = 0;
-    var currentBatch = 0;
+  it('should batch promises', () => {
+    let mostConcurrent = 0;
+    let currentBatch = 0;
 
-    return batchPromises(2, [1,2,3,4,5], function(i) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          currentBatch++;
-          if (currentBatch > mostConcurrent) mostConcurrent = currentBatch;
-          resolve(i)
-        }, 100);
-        setTimeout(function() {
-          currentBatch = 0;
-        }, 110);
-      });
-    }).then(function(res) {
+    return batchPromises(
+      2,
+      [1, 2, 3, 4, 5],
+      (i) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            currentBatch++;
+            if (currentBatch > mostConcurrent) mostConcurrent = currentBatch;
+            resolve(i);
+          }, 10);
+          setTimeout(() => {
+            currentBatch = 0;
+          }, 11);
+        })
+    ).then(() => {
       assert.equal(mostConcurrent, 2);
     });
   });
 
-  it('should reject on an error and halt execution', function() {
-    var highestPromise;
-    var error = new Error('something went wrong');
+  it('should reject on an error and halt execution', () => {
+    let highestPromise;
+    const expectedError = new Error('something went wrong');
 
-    return batchPromises(2, [1,2,3,4,5], function(i) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          if (i === 3) return reject(error);
-          highestPromise = i;
-          resolve(i);
-        }, 100);
-      });
-    })
-    .catch(function(e) {
-      assert.equal(e, error);
-      assert.equal(highestPromise, 4);
+    return batchPromises(
+      2,
+      [1, 2, 3, 4, 5],
+      (i) =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (i === 3) return reject(expectedError);
+            highestPromise = i;
+            resolve(i);
+          }, 10);
+        })
+    ).catch((actualError) => {
+      assert.equal(actualError, expectedError);
+      assert.equal(highestPromise, 2);
     });
   });
 });
